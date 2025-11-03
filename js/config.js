d8aeacacc47c61bad7a4f6d35298403ebd1cb8d8
ViralDevWander <crisysupreme@yahoo.com>
2025-11-03 18:13:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_orqbpVbWc', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6inait9lp88lg904b9fk5fomvv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_buCWe3hXn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3kb6q9noam5apta3e1svrcp3bs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
